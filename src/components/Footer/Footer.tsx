c81d341906b9537b0d19c40d1f2e9dc899a1158d
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-06-12 19:20:25 +0200

    full done HP

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import styles from './Footer.module.css';
+import React, { useState } from "react";
+import styles from "./Footer.module.css";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaSquareTwitter } from "react-icons/fa6";
-import Map from '../Map/Map';
-import FeedbackButton from '../FeedbackButton/FeedbackButton';
+import Map from "../Map/Map";
+import FeedbackButton from "../FeedbackButton/FeedbackButton";
+import { MdEmail } from "react-icons/md";
+import { FaSquarePhone } from "react-icons/fa6";
+import { GoHomeFill } from "react-icons/go";
 
 const Footer: React.FC = () => {
   const [showMap] = useState(true);
@@ -12,26 +15,46 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.contactInfo}>
-        <h3>Contact Information</h3>
-        <p>Email: carrentua@plt.com</p>
-        <p>Phone: +38066000000</p>
-        <p>Address: 123 Peremogy Street, Poltava, Ukraine</p>
+        {/* <h3>Contact us!</h3> */}
+        <p>
+          <MdEmail className={styles.iconFooter}/> carrentua@plt.com
+        </p>
+        <p>
+          <FaSquarePhone className={styles.iconFooter}/> +38066000000
+        </p>
+        <p>
+          <GoHomeFill className={styles.iconFooter}/> 33 Peremogy, Poltava, Ukraine
+        </p>
       </div>
+      {showMap && <Map />}
+      <FeedbackButton />
       <div className={styles.additionalInfo}>
         <div className={styles.socialIcons}>
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className={styles.icons}>
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.icons}
+          >
             <FaFacebookSquare className={styles.icon} />
           </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" >
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaSquareTwitter className={styles.icon} />
           </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className={styles.icons}>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.icons}
+          >
             <FaSquareInstagram className={styles.icon} />
           </a>
         </div>
       </div>
-      {showMap && <Map />}
-      <FeedbackButton />
     </footer>
   );
 };

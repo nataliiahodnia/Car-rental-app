3e4c7889ccd88b2fbcbdb241bcc5c5ea896cb228
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-06-14 14:04:45 +0200

    fixed loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import { Grid } from "react-loader-spinner";
+// import sендуі from "./Loader.module.css";
+
+export default function Loader() {
+  return (
+<Grid
+  visible={true}
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="grid-loading"
+  radius="12.5"
+  wrapperStyle={{}}
+  wrapperClass="grid-wrapper"
+  />
+  );
+}
--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,27 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchAdverts, Advert } from '../../redux/slices/advertsSlice';
-import { RootState } from '../../redux/store';
-import { ThunkDispatch } from '@reduxjs/toolkit';
-import { Action } from '@reduxjs/toolkit';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchAdverts, Advert } from "../../redux/slices/advertsSlice";
+import { RootState } from "../../redux/store";
+import { ThunkDispatch } from "@reduxjs/toolkit";
+import { Action } from "@reduxjs/toolkit";
 
-import CarList from '../../components/CarList/CarList';
-import SearchFilter from '../../components/SearchFilter/SearchFilter';
+import CarList from "../../components/CarList/CarList";
+import SearchFilter from "../../components/SearchFilter/SearchFilter";
+import Loader from "../../components/Loader/Loader";
 import styles from "./Catalog.module.css";
 
 const CatalogPage: React.FC = () => {
-  const dispatch = useDispatch<ThunkDispatch<RootState, null, Action<string>>>();
+  const dispatch =
+    useDispatch<ThunkDispatch<RootState, null, Action<string>>>();
   const [filteredAdverts, setFilteredAdverts] = useState<Advert[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [, setShowLoadMore] = useState<boolean>(false);
 
   useEffect(() => {
-    dispatch(fetchAdverts());
+    dispatch(fetchAdverts())
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (filteredAdverts.length > 0) {
+      setShowLoadMore(true);
+    } else {
+      setShowLoadMore(false);
+    }
+  }, [filteredAdverts]);
 
   return (
     <div>
       <h2 className={styles.titleCatalog}>Catalog Page</h2>
       <SearchFilter onFilter={setFilteredAdverts} />
-      <CarList filteredAdverts={filteredAdverts} pageSize={12} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <CarList filteredAdverts={filteredAdverts} pageSize={12} />
+      )}
     </div>
   );
 };

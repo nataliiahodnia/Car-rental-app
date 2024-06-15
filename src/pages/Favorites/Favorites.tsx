8c2d5249fa516d3674a01452af239f674e214cdc
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-06-15 12:11:03 +0200

    +

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectAllFavoriteCars } from '../../redux/slices/favoritesSlice';
-import { RootState } from '../../redux/store';
-import styles from './Favorites.module.css';
-import CarList from '../../components/CarList/CarList';
-import { Advert } from '../../redux/slices/advertsSlice';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllFavoriteCars } from "../../redux/slices/favoritesSlice";
+import { RootState } from "../../redux/store";
+import styles from "./Favorites.module.css";
+import CarList from "../../components/CarList/CarList";
+import { Advert } from "../../redux/slices/advertsSlice";
+import { FaRegFaceSadTear } from "react-icons/fa6";
 
 const FavoritesPage: React.FC = () => {
-  const favoriteCars = useSelector((state: RootState) => selectAllFavoriteCars(state));
+  const favoriteCars = useSelector((state: RootState) =>
+    selectAllFavoriteCars(state)
+  );
 
   return (
     <div className={styles.favoritesPage}>
       <h1 className={styles.titleFavorite}>My Favorite Cars</h1>
-      <CarList filteredAdverts={favoriteCars as Advert[]} pageSize={12} />
+      {favoriteCars.length === 0 ? (
+        <div className={styles.noFavorites}>
+          <FaRegFaceSadTear  className={styles.noFavoritesIcon} />
+          <p className={styles.noFavoritesMessage}>
+            You don't have any favorite cars yet. Add some cars to your
+            favorites!
+          </p>
+        </div>
+      ) : (
+        <CarList filteredAdverts={favoriteCars as Advert[]} pageSize={12} />
+      )}
     </div>
   );
 };
